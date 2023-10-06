cca773dc646c4b803404e8bb6aacfb7973857abf
Martínez_Carlos <20223tn066@utez.edu.mx>
2023-10-05 21:07:39 -0600

    Creación de CURP

--- ActsNode4B_js/Act4B_2/index.js
+++ ActsNode4B_js/Act4B_2/index.js
@@ -45,6 +45,76 @@ app.get('/api/agenda/:id', (req, res) => {
 });
 
 app.post('/api/agenda', (req, res) => {
+    let name = req.body.name;
+    let firstLastname = req.body.firstLastname;
+    let secondLastname = req.body.secondLastname;
+    let birthdate = req.body.birthdate;
+    let sex = req.body.sex;
+    let state = req.body.state;
+
+    //Inicial y primer vocal del primer apellido
+    const vocales = "aeiou";
+    let vocal = "";
+    let ape1 = firstLastname.toLowerCase();
+    for (let i = 0; i < ape1.length; i++) {
+        if (vocales.includes(ape1[i])) {
+            vocal = ape1[i];
+        }
+    }
+    const part1 = ape1.charAt(0) + vocal.charAt(0) + secondLastname.charAt(0) + name.charAt(0);
+
+    //Separar fecha
+    const fecha = birthdate.split('/');
+    let dia = fecha[0].padStart(2, '0');
+    let mes = fecha[1].padStart(2, '0');
+    let año = fecha[2].slice(-2);
+    const part2 = año + mes + dia;
+
+    //Sexo
+    const part3 = sex;
+
+    //Entidad federativa
+    const part4 = state.substring(0, 2);
+
+
+    //Primer consonante interna de apellidos y nombres
+    const n1 = name.toLowerCase();
+    const a1 = firstLastname.toLowerCase();
+    const a2 = secondLastname.toLowerCase();
+    const consonantes = "bcdfghjklmnñpqrstvxzwy";
+    let nombre = "";
+    let apellido1 = "";
+    let apellido2 = "";
+
+    for (let i = 0; i < name.length; i++) {
+        if (consonantes.includes(n1[i])) {
+            nombre += name[i];
+        }
+    }
+
+    for (let i = 0; i < firstLastname.length; i++) {
+        if (consonantes.includes(a1[i])) {
+            apellido1 += firstLastname[i];
+        }
+    }
+
+    for (let i = 0; i < secondLastname.length; i++) {
+        if (consonantes.includes(a2[i])) {
+            apellido2 += secondLastname[i];
+        }
+    }
+
+    const part5 = apellido1.charAt(1) + apellido2.charAt(1) + nombre.charAt(1)
+
+    //Números random
+    let num1 = Math.floor(Math.random() * 10);
+    let num2 = Math.floor(Math.random() * 10);
+    let part6 = "" + num1 + num2;
+
+    //CURP
+    let curpUnion = part1 + part2 + part3 + part4 + part5 + part6;
+    let curpFinal = curpUnion.toUpperCase();
+
     const agenda = {
         id: agenda.length + 1,
         name: req.body.name,
@@ -52,59 +122,10 @@ app.post('/api/agenda', (req, res) => {
         secondLastname: req.body.secondLastname,
         birthdate: req.body.birthdate,
         sex: req.body.sex,
-        state: req.body.state
+        state: req.body.state,
+        CURP: curpFinal,
     }
 
-    // Zona de funciones
-    function separarFecha(agenda) {
-        const fecha = agenda.birthdate.split('/');
-        const dia = fecha[0];
-        const mes = fecha[1];
-        let año = fecha[2].slice(-2);
-        return { dia, mes, año };
-    }
-
-    function numeroRandom() {
-        var num1 = Math.floor(Math.random() * 10);
-        var num2 = Math.floor(Math.random() * 10);
-        const num = "" + num1 + num2;
-        return num;
-    }
-
-    function entidad(agenda) {
-        const estado = agenda.state.substring(0, 2);
-        return estado;
-    }
-
-    function consonante(agenda) {
-        const name = agenda.name.toLowerCase();
-        const firstLastname = agenda.firstLastname.toLowerCase();
-        const secondLastname = agenda.secondLastname.toLowerCase();
-        const consonantes = "bcdfghjklmnñpqrstvxzwy";
-        let nombre = "";
-        let apellido1 = "";
-        let apellido2 = "";
-
-        for (let i = 0; i < name.length; i++) {
-            if (consonantes.includes(name[i])) {
-                nombre += name[i];
-            }
-        }
-
-        for (let i = 0; i < firstLastname.length; i++) {
-            if (consonantes.includes(firstLastname[i])) {
-                apellido1 += firstLastname[i];
-            }
-        }
-
-        for (let i = 0; i < secondLastname.length; i++) {
-            if (consonantes.includes(secondLastname[i])) {
-                apellido2 += secondLastname[i];
-            }
-        }
-
-        return apellido1.charAt(1) + apellido2.charAt(1) + nombre.charAt(1);
-    }
     agenda.push(agenda);
     res.json(agenda);
 });
