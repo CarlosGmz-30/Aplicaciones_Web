848610d85febb2079fc33ff255d4c27ee17a50bf
Martínez_Carlos <20223tn066@utez.edu.mx>
2023-10-06 14:31:31 -0600

    PATCH y PUT

--- ActsNode4B_js/Act4B_2/index.js
+++ ActsNode4B_js/Act4B_2/index.js
@@ -192,5 +192,194 @@ app.post('/api/agenda', (req, res) => {
     res.json(nuevoContacto);
 });
 
+app.patch('/api/agenda/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const contact = req.body;
+
+    //Validar si existe el contacto
+    const user = agenda.find(c => c.id === id);
+    if (!user) {
+        return res.status(404).send('El contacto con el ID dado no fue encontrado');
+    }
+
+    //Actualizar nombre del contacto
+    if (contact.name) {
+        user.name = contact.name;
+    }
+
+    //Actualizar apellido paterno del contacto
+    if (contact.firstLastname) {
+        user.firstLastname = contact.firstLastname;
+    }
+
+    //Actualizar apellido materno del contacto
+    if (contact.secondLastname) {
+        user.secondLastname = contact.secondLastname;
+    }
+
+    //Actualizar fecha de nacimiento del contacto
+    if (contact.birthdate) {
+        user.birthdate = contact.birthdate;
+    }
+
+    //Actualizar sexo del contacto
+    if (contact.sex) {
+        user.sex = contact.sex;
+    }
+
+    //Actualizar estado del contacto
+    if (contact.state) {
+        user.state = contact.state;
+    }
+
+    // Actualizar el CURP con los nuevos datos
+    const vocales = "aeiou";
+    let vocal = "";
+    let ape1 = user.firstLastname;
+
+    for (let i = 0; i < ape1.length; i++) {
+        if (vocales.includes(ape1[i].toLowerCase())) {
+            vocal = ape1[i];
+            break;
+        }
+    }
+
+    const part1 = ape1.charAt(0) + vocal.charAt(0) + user.secondLastname.charAt(0) + user.name.charAt(0);
+    const fecha = user.birthdate.split('/');
+    let dia = fecha[0].padStart(2, '0');
+    let mes = fecha[1].padStart(2, '0');
+    let año = fecha[2].slice(-2);
+    const part2 = año + mes + dia;
+    const part3 = user.sex;
+    const part4 = user.state.substring(0, 2);
+
+    const n1 = user.name.toLowerCase();
+    const a1 = user.firstLastname.toLowerCase();
+    const a2 = user.secondLastname.toLowerCase();
+    const consonantes = "bcdfghjklmnñpqrstvxzwy";
+    let nombre = "";
+    let apellido1 = "";
+    let apellido2 = "";
+
+    for (let i = 0; i < user.name.length; i++) {
+        if (consonantes.includes(n1[i])) {
+            nombre += user.name[i];
+        }
+    }
+
+    for (let i = 0; i < user.firstLastname.length; i++) {
+        if (consonantes.includes(a1[i])) {
+            apellido1 += user.firstLastname[i];
+        }
+    }
+
+    for (let i = 0; i < user.secondLastname.length; i++) {
+        if (consonantes.includes(a2[i])) {
+            apellido2 += user.secondLastname[i];
+        }
+    }
+
+    const part5 = apellido1.charAt(1) + apellido2.charAt(1) + nombre.charAt(1);
+
+    let num1 = Math.floor(Math.random() * 10);
+    let num2 = Math.floor(Math.random() * 10);
+    let part6 = "" + num1 + num2;
+
+    let curpUnion = part1 + part2 + part3 + part4 + part5 + part6;
+    let curpFinal = curpUnion.toUpperCase();
+
+    user.CURP = curpFinal;
+
+    res.json(user);
+});
+
+app.put('/api/agenda/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const contact = req.body;
+
+    const user = agenda.find(c => c.id === id);
+    if (!user) {
+        return res.status(404).send('El contacto con el ID dado no fue encontrado');
+    }
+
+    //Actualizar nombre del contacto
+    user.name = contact.name;
+
+    //Actualizar apellido paterno del contacto
+    user.firstLastname = contact.firstLastname;
+
+    //Actualizar apellido materno del contacto
+    user.secondLastname = contact.secondLastname;
+
+    //Actualizar fecha de nacimiento del contacto
+    user.birthdate = contact.birthdate;
+
+    //Actualizar sexo del contacto
+    user.sex = contact.sex;
+
+    //Actualizar estado del contacto
+    user.state = contact.state;
+
+    // Actualizar el CURP con los nuevos datos
+    const vocales = "aeiou";
+    let vocal = "";
+    let ape1 = user.firstLastname;
+
+    for (let i = 0; i < ape1.length; i++) {
+        if (vocales.includes(ape1[i].toLowerCase())) {
+            vocal = ape1[i];
+            break;
+        }
+    }
+
+    const part1 = ape1.charAt(0) + vocal.charAt(0) + user.secondLastname.charAt(0) + user.name.charAt(0);
+    const fecha = user.birthdate.split('/');
+    let dia = fecha[0].padStart(2, '0');
+    let mes = fecha[1].padStart(2, '0');
+    let año = fecha[2].slice(-2);
+    const part2 = año + mes + dia;
+    const part3 = user.sex;
+    const part4 = user.state.substring(0, 2);
+
+    const n1 = user.name.toLowerCase();
+    const a1 = user.firstLastname.toLowerCase();
+    const a2 = user.secondLastname.toLowerCase();
+    const consonantes = "bcdfghjklmnñpqrstvxzwy";
+    let nombre = "";
+    let apellido1 = "";
+    let apellido2 = "";
+
+    for (let i = 0; i < user.name.length; i++) {
+        if (consonantes.includes(n1[i])) {
+            nombre += user.name[i];
+        }
+    }
+
+    for (let i = 0; i < user.firstLastname.length; i++) {
+        if (consonantes.includes(a1[i])) {
+            apellido1 += user.firstLastname[i];
+        }
+    }
+
+    for (let i = 0; i < user.secondLastname.length; i++) {
+        if (consonantes.includes(a2[i])) {
+            apellido2 += user.secondLastname[i];
+        }
+    }
+
+    const part5 = apellido1.charAt(1) + apellido2.charAt(1) + nombre.charAt(1);
+
+    let num1 = Math.floor(Math.random() * 10);
+    let num2 = Math.floor(Math.random() * 10);
+    let part6 = "" + num1 + num2;
+
+    let curpUnion = part1 + part2 + part3 + part4 + part5 + part6;
+    let curpFinal = curpUnion.toUpperCase();
+
+    user.CURP = curpFinal;
+
+    res.json(user);
+});
+
 const port = process.env.PORT || 80;
 app.listen(port, () => console.log(`Listen en el puerto ${port}`));
