e9ea7cc099c61b0ecc196c13e249dcc776832f4b
Carlos <20223tn066@utez.edu.mx>
2023-10-25 14:47:26 -0600

    Cambios

--- ActsSpringBoot/Ejemplo4B/src/main/java/mx/edu/utez/Ejemplo4B/service/impl/ImplEmployee.java
+++ ActsSpringBoot/Ejemplo4B/src/main/java/mx/edu/utez/Ejemplo4B/service/impl/ImplEmployee.java
@@ -3,26 +3,30 @@ package mx.edu.utez.Ejemplo4B.service.impl;
 import mx.edu.utez.Ejemplo4B.model.dao.DaoEmployee;
 import mx.edu.utez.Ejemplo4B.model.entity.BeanEmployee;
 import mx.edu.utez.Ejemplo4B.service.IEmployee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 // Este se va encargar de traer los métodos
 
 @Service
 public class ImplEmployee implements IEmployee {
+    @Autowired
     // Traemos los métodos
     private DaoEmployee daoEmployee;
-
+    @Transactional
     @Override
     public BeanEmployee save(BeanEmployee employee) {
         return daoEmployee.save(employee);
     }
-
+    @Transactional(readOnly = true)
     @Override
     public BeanEmployee findById(Integer id) {
         return daoEmployee.findById(id).orElse(null);
         //                              Por si no existe returna un nulo
     }
 
+    @Transactional
     @Override
     public void delete(BeanEmployee employee) {
         daoEmployee.delete(employee);
