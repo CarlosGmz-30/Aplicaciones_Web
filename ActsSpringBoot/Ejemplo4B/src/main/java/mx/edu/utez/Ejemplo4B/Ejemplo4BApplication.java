4960ae6a11c72f4425c0a0551c6020a72fd02163
Carlos <20223tn066@utez.edu.mx>
2023-10-25 13:34:53 -0600

    ActsSpringsBoot

---
+++ ActsSpringBoot/Ejemplo4B/src/main/java/mx/edu/utez/Ejemplo4B/Ejemplo4BApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.Ejemplo4B;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejemplo4BApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejemplo4BApplication.class, args);
+	}
+
+}
---
+++ ActsSpringBoot/Ejemplo4B/src/test/java/mx/edu/utez/Ejemplo4B/Ejemplo4BApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.Ejemplo4B;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejemplo4BApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
