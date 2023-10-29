ebb756742da01ae071fe2917e1c51d31318f3631
Carlos <20223tn066@utez.edu.mx>
2023-10-28 23:24:14 -0600

    Cambios

--- ActsNode4B_js/Act4B_4/src/controllers/alumnos.controller.js
+++ ActsNode4B_js/Act4B_4/src/controllers/alumnos.controller.js
@@ -21,22 +21,35 @@ const createAlumno = async (req, res) => {
     res.send({ filas });
 };
 
-const updateAlumnos = async (req, res) => {
+const updateAlumno = async (req, res) => {
     const { idM } = req.params;
     const { matricula, nombre, direccion, telefono, id_carrera } = req.body;
 
     try {
-        const [verification] = await pool.query('SELECT * FROM alumnos WHERE id = ?', [idM]);
-        if (verification.lenght == 0) {
+        const [verification] = await pool.query('SELECT * FROM alumnos WHERE matricula = ?', [idM]);
+        if (verification.length == 0) {
             return res.status(404).json({ msj: "Alumno no Encontrado" });
         } else {
-            await pool.query('UPDATE alumno SET ', [matricula, nombre, direccion, telefono, id_carrera, idM]);
+            await pool.query('UPDATE alumnos SET matricula = ?, nombre = ?, direccion = ?, telefono = ?, id_carrera = ? WHERE matricula = ?', [matricula, nombre, direccion, telefono, id_carrera, idM]);
         }
 
-        res.send("Empleado actualizado correctamente");
+        res.send("Alumno actualizado correctamente");
     } catch (error) {
-        console.error("Error al actualizar al empleado", error);
-        res.status(500).json({ "msj": "Error al actualizar al empleado" });
+        console.error("Error al actualizar al alumno", error);
+        res.status(500).json({ "msj": "Error al actualizar al alumno" });
     };
+};
 
+const deleteAlumno = async (req, res) => {
+    const matricula = req.params.matricula;
+    const [rows] = await pool.query('DELETE FROM alumnos WHERE matricula = ?', [matricula]);
+    if (rows.affectedRows > 0) {
+        return res.json("Alumno eliminado exitosamente");
+    } else {
+        return res.json("Alumno no econtrado en el sistema")
+    }
+}
+
+module.exports = {
+    getAlumnos, updateAlumno, deleteAlumno, createAlumno, getOne
 };
