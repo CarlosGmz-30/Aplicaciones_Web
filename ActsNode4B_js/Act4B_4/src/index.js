5c74f976426f29e04dc0155f08176a863ce57d9d
Carlos <20223tn066@utez.edu.mx>
2023-10-28 23:41:42 -0600

    Ya funciona

--- ActsNode4B_js/Act4B_4/src/index.js
+++ ActsNode4B_js/Act4B_4/src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const universityRoutes = require('./routes/alumnos.routes.js');
+const indexRoutes = require('./routes/index.routes.js');
 
 const app = express();
 app.use(express.json());
 console.clear();
+app.use("/api", indexRoutes);
+app.use("/api", universityRoutes);
 
 
 app.listen(8080);
