918c7d73f9d8f35ebd88855e59b2d3c965fa309f
Carlos <20223tn066@utez.edu.mx>
2023-11-10 14:26:56 -0600

    Interfaces de Client y Employee

--- ActsSpringBoot/BancoB/src/main/java/mx/edu/utez/BancoB/Service/IClient.java
+++ ActsSpringBoot/BancoB/src/main/java/mx/edu/utez/BancoB/Service/IClient.java
@@ -1,2 +1,18 @@
-package mx.edu.utez.BancoB.Service;public interface IClient {
+package mx.edu.utez.BancoB.Service;
+
+import mx.edu.utez.BancoB.Model.dto.DtoClient;
+import mx.edu.utez.BancoB.Model.entity.clientBean;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface IClient {
+    clientBean save(DtoClient client);
+
+    clientBean findById(Integer id_client);
+
+    void delete(clientBean client);
+
+    @Transactional(readOnly = true)
+    List<clientBean> findAll();
 }
--- ActsSpringBoot/BancoB/src/main/java/mx/edu/utez/BancoB/Service/IEmployee.java
+++ ActsSpringBoot/BancoB/src/main/java/mx/edu/utez/BancoB/Service/IEmployee.java
@@ -1,2 +1,18 @@
-package mx.edu.utez.BancoB.Service;public interface IEmployee {
+package mx.edu.utez.BancoB.Service;
+
+import mx.edu.utez.BancoB.Model.dto.DtoEmployee;
+import mx.edu.utez.BancoB.Model.entity.employeeBean;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface IEmployee {
+    employeeBean save(DtoEmployee employee);
+
+    employeeBean findById(Integer id_employee);
+
+    void delete(employeeBean employee);
+
+    @Transactional(readOnly = true)
+    List<employeeBean> findAll();
 }
