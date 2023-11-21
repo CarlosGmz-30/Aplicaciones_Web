f00288664b45433ad940738d1a8ef17fbf2bc20f
Carlos <20223tn066@utez.edu.mx>
2023-11-21 08:50:52 -0600

    Examen Práctica UII

---
+++ ActsSpringBoot/ExamenUII/src/main/java/mx/edu/utez/ExamenUII/ExamenUiiApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.ExamenUII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenUiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenUiiApplication.class, args);
+	}
+
+}
