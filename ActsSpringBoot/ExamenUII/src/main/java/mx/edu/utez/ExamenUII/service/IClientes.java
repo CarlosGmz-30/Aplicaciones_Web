0e6baf689713e9cc0605503baf6e383fc0863910
Carlos <20223tn066@utez.edu.mx>
2023-11-21 10:04:07 -0600

    Cambios en el IClientes

--- ActsSpringBoot/ExamenUII/src/main/java/mx/edu/utez/ExamenUII/service/IClientes.java
+++ ActsSpringBoot/ExamenUII/src/main/java/mx/edu/utez/ExamenUII/service/IClientes.java
@@ -1,13 +1,13 @@
 package mx.edu.utez.ExamenUII.service;
 
-import mx.edu.utez.ExamenUII.model.dto.DtoCoches;
+import mx.edu.utez.ExamenUII.model.dto.DtoClientes;
 import mx.edu.utez.ExamenUII.model.entity.ClientesBean;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface IClientes {
-    ClientesBean save(DtoCoches bean);
+    ClientesBean save(DtoClientes bean);
 
     ClientesBean findById(Integer id_cliente);
 
