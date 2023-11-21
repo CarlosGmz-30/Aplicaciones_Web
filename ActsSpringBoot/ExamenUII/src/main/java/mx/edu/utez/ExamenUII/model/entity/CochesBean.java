61e1f264df5051b4071086e9c458c42556d9f835
Carlos <20223tn066@utez.edu.mx>
2023-11-21 09:06:18 -0600

    Cambios

--- ActsSpringBoot/ExamenUII/src/main/java/mx/edu/utez/ExamenUII/model/entity/CochesBean.java
+++ ActsSpringBoot/ExamenUII/src/main/java/mx/edu/utez/ExamenUII/model/entity/CochesBean.java
@@ -1,4 +1,16 @@
 package mx.edu.utez.ExamenUII.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Entity
+@Table(name = "coches")
 public class CochesBean {
 }
