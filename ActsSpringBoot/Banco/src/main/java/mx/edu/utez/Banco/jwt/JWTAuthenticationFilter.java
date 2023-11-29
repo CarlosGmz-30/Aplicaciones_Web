421e8cfe91438e6182b2ebddbb6396a6e90ae835
Carlos gmz <20223tn066@utez.edu.mx>
2023-11-29 14:23:22 -0600

    JWTAutheticationFilter

--- ActsSpringBoot/Banco/src/main/java/mx/edu/utez/Banco/jwt/JWTAuthenticationFilter.java
+++ ActsSpringBoot/Banco/src/main/java/mx/edu/utez/Banco/jwt/JWTAuthenticationFilter.java
@@ -1,5 +1,27 @@
 package mx.edu.utez.Banco.jwt;
 
-public class JWTAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
+
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String token = getTokenFromRequest();
+    }
+
+    private String getTokenFromRequest(HttpServletRequest request) {
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (StringUtils.hasText(authHeader) && authHeader.startsWith("Bearer ")) {
+            return authHeader.substring(7);
+        }
+        return null;
+    }
 }
