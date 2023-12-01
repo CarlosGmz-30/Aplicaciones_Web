94a564fd0de25712d3eb8bd426044549a309930a
Carlos <20223tn066@utez.edu.mx>
2023-12-01 14:03:53 -0600

    Clases IComentarios, y IUsuarops terminadas

--- ActsSpringBoot/ExamenRecupera/src/main/java/mx/edu/utez/ExamenRecupera/service/IComentarios.java
+++ ActsSpringBoot/ExamenRecupera/src/main/java/mx/edu/utez/ExamenRecupera/service/IComentarios.java
@@ -1,4 +1,18 @@
 package mx.edu.utez.ExamenRecupera.service;
 
+import mx.edu.utez.ExamenRecupera.model.dto.DtoComentarios;
+import mx.edu.utez.ExamenRecupera.model.entity.comentariosBean;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 public interface IComentarios {
+    comentariosBean save(DtoComentarios comentarios);
+
+    comentariosBean findById(Integer id_comentarios);
+
+    void delete(comentariosBean comentarios);
+
+    @Transactional(readOnly = true)
+    List<comentariosBean> findAll();
 }
--- ActsSpringBoot/ExamenRecupera/src/main/java/mx/edu/utez/ExamenRecupera/service/IUsuarios.java
+++ ActsSpringBoot/ExamenRecupera/src/main/java/mx/edu/utez/ExamenRecupera/service/IUsuarios.java
@@ -1,4 +1,18 @@
 package mx.edu.utez.ExamenRecupera.service;
 
+import mx.edu.utez.ExamenRecupera.model.dto.DtoUsuarios;
+import mx.edu.utez.ExamenRecupera.model.entity.usuariosBean;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 public interface IUsuarios {
+    usuariosBean save(DtoUsuarios usuarios);
+
+    usuariosBean findById(Integer id_usuarios);
+
+    void delete(usuariosBean usuarios);
+
+    @Transactional(readOnly = true)
+    List<usuariosBean> findALL();
 }
