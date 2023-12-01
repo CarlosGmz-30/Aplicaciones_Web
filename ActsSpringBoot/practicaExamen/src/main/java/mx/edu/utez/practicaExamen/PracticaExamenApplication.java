4458c82417d6eff60c29c7fb2c98c180ba5b8f1b
Carlos <20223tn066@utez.edu.mx>
2023-11-30 21:36:51 -0600

    proyecto de práctica para el examen recupera

---
+++ ActsSpringBoot/practicaExamen/src/main/java/mx/edu/utez/practicaExamen/PracticaExamenApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.practicaExamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaExamenApplication.class, args);
+	}
+
+}
---
+++ ActsSpringBoot/practicaExamen/src/test/java/mx/edu/utez/practicaExamen/PracticaExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.practicaExamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
