f98187543d64c0b2f35d1a63f4326cda3a96ba87
Carlos <20223tn066@utez.edu.mx>
2023-12-01 14:00:28 -0600

    Terminamos las clases Dto de Usuario y COmentarios

--- ActsSpringBoot/ExamenRecupera/src/main/java/mx/edu/utez/ExamenRecupera/model/dto/DtoComentarios.java
+++ ActsSpringBoot/ExamenRecupera/src/main/java/mx/edu/utez/ExamenRecupera/model/dto/DtoComentarios.java
@@ -1,4 +1,15 @@
 package mx.edu.utez.ExamenRecupera.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@ToString
+@Builder
 public class DtoComentarios {
+    private Integer id_comentario;
+    private String comentario;
 }
--- ActsSpringBoot/ExamenRecupera/src/main/java/mx/edu/utez/ExamenRecupera/model/dto/DtoUsuarios.java
+++ ActsSpringBoot/ExamenRecupera/src/main/java/mx/edu/utez/ExamenRecupera/model/dto/DtoUsuarios.java
@@ -1,4 +1,17 @@
 package mx.edu.utez.ExamenRecupera.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@ToString
+@Builder
 public class DtoUsuarios {
+    private Integer id_usuarios;
+    private String nombre;
+    private String email;
 }
+
