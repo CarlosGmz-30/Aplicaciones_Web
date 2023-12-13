9273514cb8a32b71f41c9c8694155dd33edeeaf6
Carlos gmz <20223tn066@utez.edu.mx>
2023-12-12 23:09:58 -0600

    Actividad de CRUDS

--- ActsSpringBoot/BranchCrud/js/index.js
+++ ActsSpringBoot/BranchCrud/js/index.js
@@ -1,8 +1,8 @@
-document.getElementById('forms').addEventListener('submit', function (event) {
+document.getElementById('forms1').addEventListener('submit', function (event) {
     event.preventDefault();
-    let name = document.getElementById('name').value;
-    let address = document.getElementById('address').value;
-    let code = document.getElementById('code').value;
+    let name = document.getElementById('name_post').value;
+    let address = document.getElementById('address_post').value;
+    let code = document.getElementById('code_post').value;
 
     fetch('http://localhost:8080/api/v1/branch/', {
         method: 'POST',
@@ -18,9 +18,9 @@ document.getElementById('forms').addEventListener('submit', function (event) {
             document.getElementById('response').innerHTML = JSON.stringify(data);
             alert("Usuario creado exitosamente");
 
-            document.getElementById('name').value = "";
-            document.getElementById('address').value = "";
-            document.getElementById('code').value = "";
+            document.getElementById('name_post').value = "";
+            document.getElementById('address_post').value = "";
+            document.getElementById('code_post').value = "";
         })
         .catch(error => {
             console.error('Error:', error);
@@ -32,46 +32,47 @@ document.getElementById('forms').addEventListener('submit', function (event) {
 
 document.getElementById('forms2').addEventListener('submit', function (event) {
     event.preventDefault();
-    let id = document.getElementById('id').value;
-    let name = document.getElementById('name').value;
-    let address = document.getElementById('address').value;
-    let code = document.getElementById('code').value;
+    let id_branch = document.getElementById('id_put').value;
+    let name = document.getElementById('name_put').value;
+    let address = document.getElementById('address_put').value;
+    let code = document.getElementById('code_put').value;
 
-    fetch(`http://localhost:8080/api/v1/branch/${id}`, {
+    let url = 'http://localhost:8080/api/v1/branch/'
+    let url1 = url + id_branch;
+    fetch(url1, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ name, address, code })
+        body: JSON.stringify({ id_branch, address, code, name })
     })
         .then(response => response.json())
         .then(data => {
             document.getElementById('response').innerHTML = JSON.stringify(data);
             alert("Usuario actualizado exitosamente");
 
-            document.getElementById('id').value = "";
-            document.getElementById('name').value = "";
-            document.getElementById('address').value = "";
-            document.getElementById('code').value = "";
+            document.getElementById('id_put').value = "";
+            document.getElementById('name_put').value = "";
+            document.getElementById('address_put').value = "";
+            document.getElementById('code_put').value = "";
         })
         .catch(error => {
             console.error('Error:', error);
             alert("Hubo un error al actualizar el usuario");
-            document.getElementById('id').value = "";
-            document.getElementById('name').value = "";
-            document.getElementById('address').value = "";
-            document.getElementById('code').value = "";
+            document.getElementById('id_put').value = "";
+            document.getElementById('name_put').value = "";
+            document.getElementById('address_put').value = "";
+            document.getElementById('code_put').value = "";
         });
 });
 
-
+/*
 document.getElementById('borrar').addEventListener('submit', function (event) {
     event.preventDefault();
     let id = document.getElementById('id_borrar').value;
 
     let url = 'http://localhost:8080/api/v1/branch/'
     let url1 = url + id;
-    console.log(url1);
     fetch(url1, {
         method: 'DELETE',
         headers: {
@@ -84,4 +85,4 @@ document.getElementById('borrar').addEventListener('submit', function (event) {
             alert("Usuario borrado exitosamente");
         })
 
-})
+})*/
